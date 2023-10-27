3ff0fb3d37036179dca13e7eadc058e107be1e8e
root <root@localhost.localdomain>
2023-10-27 13:44:09 +0500

    скрины заданий 1 2

--- labs/lab1/task4.c
+++ labs/lab1/task4.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 
-int main(int argc, char *argv[])
+int main(int argc, char *argv[]) //создание массива и записывания переменных
 {
-    for (int i = 0; i < argc; i++)
+    for (int i = 0; i < argc; i++) //проходит по всем элементам
     {
-        printf("%s \n", argv[i]);
+        printf("%s \n", argv[i]); // выводит содержимое массива
+
     }
     return 0;
 }
